5b904d33af57a170073f35fb4012103db2f4a15a
onlysavior <onlysavior@foxmail.com>
2013-12-01 23:52:53 +0800

    trie tree
    Signed-off-by: onlysavior <onlysavior@foxmail.com>

---
+++ src/main/java/onlysaviorcommon/datastruct/AbstractTrie.java
@@ -0,0 +1,63 @@
+package onlysaviorcommon.datastruct;
+
+import sun.nio.cs.StandardCharsets;
+
+import java.nio.ByteBuffer;
+
+/**
+ * base of all trie implement
+ * @param <V>
+ */
+public abstract class AbstractTrie<V> implements Trie<V> {
+    final boolean _caseInsensitive;
+
+    protected AbstractTrie(boolean insensitive)
+    {
+        _caseInsensitive=insensitive;
+    }
+
+    @Override
+    public boolean put(V v)
+    {
+        return put(v.toString(),v);
+    }
+
+    @Override
+    public V remove(String s)
+    {
+        V o=get(s);
+        put(s,null);
+        return o;
+    }
+
+    @Override
+    public V get(String s)
+    {
+        return get(s,0,s.length());
+    }
+
+    @Override
+    public V get(ByteBuffer b)
+    {
+        return get(b,0,b.remaining());
+    }
+
+    @Override
+    public V getBest(String s)
+    {
+        return getBest(s,0,s.length());
+    }
+
+    @Override
+    public V getBest(byte[] b, int offset, int len)
+    {
+        return getBest(new String(b,offset,len, StandardCharsets.ISO_8859_1));
+    }
+
+    @Override
+    public boolean isCaseInsensitive()
+    {
+        return _caseInsensitive;
+    }
+
+}
